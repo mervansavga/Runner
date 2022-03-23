93457aa9fdba3106fc687e7c79058375eff3e752
mervansavga <mervan.savga@bertec.com>
2022-03-23 13:06:00 +0300

    General project structure

---
+++ Runner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //TODO Console prompt user to select SumOfMultiple or SequenceAnalysis
+
+            //TODO After user select get parameters from console and output the result
+        }
+    }
+}
